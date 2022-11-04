ae68c758811de52d1133ef766fc7f15e5291a1b1
Joao Paulo <joaopaulojeronimo00@gmail.com>
2022-11-04 16:42:16 -0300

    criação projeto da algaworks

---
+++ src/main/java/com/algaworks/algalog/AlgalogApiWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogApiWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogApiWorkshopApplication.class, args);
+	}
+
+}
