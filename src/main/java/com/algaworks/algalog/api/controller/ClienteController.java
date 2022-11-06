ab80c5337230f0ef3fdc5f00b640675065027ec8
JoaoPauloJeronimo <joaopaulojeronimo00@gmail.com>
2022-11-06 18:15:47 -0300

    client CRUD complete

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,20 +3,66 @@ package com.algaworks.algalog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> findAll() {		
-		return clienteRepository.findByNome("João Paulo Jerônimo");
+		return clienteRepository.findAll();
 	}
-}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id) {
+		/*Optional<Cliente> cliente = clienteRepository.findById(id);
+		if(cliente.isPresent()) {
+			 return ResponseEntity.ok(cliente.get());
+		}*/		 
+		return clienteRepository.findById(id)
+				.map(cliente -> ResponseEntity.ok(cliente))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente add(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(id);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+ }
---
+++ src/main/resources/db/migration/V0611__insere-itens-na-tabela.sql
@@ -0,0 +1,7 @@
+INSERT INTO cliente (nome, email, telefone) VALUES ('joao', 'joao@gmail.com', '66666666666');
+INSERT INTO cliente (nome, email, telefone) VALUES ('maria', 'maria@gmail.com', '1111111111');
+INSERT INTO cliente (nome, email, telefone) VALUES ('amandona', 'amandona@gmail.com', '22222222222');
+INSERT INTO cliente (nome, email, telefone) VALUES ('amamandinha', 'amandinha@gmail.com', '33333333333');
+
+
+
